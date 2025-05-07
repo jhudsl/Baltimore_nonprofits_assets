92f847e1b020f06efd7bb4162fa93d7562c32332
carriewright11 <cwright2@fredhutch.org>
2025-05-07 16:56:13 -0400

    remove excess script

--- Geo_coding.R
+++
@@ -1,6 +0,0 @@
-library(ggmap)
-library(dplyr)
-IRS <- readRDS(file = "New_version_data/IRS_data_before_lat_long.rds")
-geos <- IRS %>% pull(address) %>% geocode()
-saveRDS(geos, file = "New_version_data/geos.rds")
-
